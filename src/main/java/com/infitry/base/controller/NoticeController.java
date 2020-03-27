7313528aba8ff4a49c2a962501af46ea1b05c67a
leesw <leesw@DESKTOP-UT2THB4>
2020-03-27 15:52:47 +0900

    [2020-03-27] - 컨트롤러 생성

---
+++ src/main/java/com/infitry/base/controller/NoticeController.java
@@ -0,0 +1,16 @@
+package com.infitry.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/notice")
+public class NoticeController {
+	
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public String main() {
+		System.out.println("notice list");
+		return "/notice/list";
+	}
+}
