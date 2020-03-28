9fd8a91b1ac30aef11b41f027626a9e43ca4d6a9
leesw <leesw@DESKTOP-UT2THB4>
2020-03-28 17:50:57 +0900

    [2020-03-28] - spring boot + jsp + vue.js 결정.

--- src/main/java/com/infitry/base/controller/MainController.java
+++ src/main/java/com/infitry/base/controller/MainController.java
@@ -2,12 +2,11 @@ package com.infitry.base.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value = "/")
 	public String main() {
 		return "index";
 	}
