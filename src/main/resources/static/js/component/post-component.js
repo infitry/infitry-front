95971da0fd5c50c4c308ed9b2b6e3dd8b8a05029
leesw <leesw@DESKTOP-UT2THB4>
2020-04-14 12:03:06 +0900

    [2020-04-14] - 메인 개편 - 포스트 리스트 컴포넌트 생성 - 모든페이지 컴포넌트화

---
+++ src/main/resources/static/js/component/post-component.js
@@ -0,0 +1,71 @@
+Vue.component('post-component', {
+	template: '#postTemplate',
+	data: function() {
+		return {
+			list: [],
+	  		baseUrl: '/blog/post/axios-list',
+	  		categorySeq: $('#blogPostCategorySeq').val(),
+	  		page: 0,
+	  		size: 3,
+	  		last: false,
+			first: true
+		}	
+	},
+	filters: {
+		dateFormat: function(value) {
+		  	if (!value) {
+		  		return '';
+		  	}
+		  	return moment(value).format("YYYY .MM .DD");
+		}
+	},
+	methods: {
+		getList: function() {
+ 			 var vm = this;
+ 			 axios.get(this.baseUrl, {
+ 				 params: {
+ 					pageNumber: this.page,
+ 					pageSize: this.size,
+ 					blogPostCategorySeq: this.categorySeq
+ 				 },
+ 				 headers: { 
+ 					 'requestType': 'axios'
+ 				 }
+ 			}).then(function(response) {
+ 	        	vm.list = response.data.content;
+ 	        	vm.page = response.data.number;
+ 	        	vm.last = response.data.last;
+ 	        	vm.first = response.data.first;
+ 	        	if (!vm.last) {
+ 	        		$('#pagination').show();
+ 	        	}
+ 	        	if (response.data.totalElements == 0) {
+ 	        		$('#listNull').show();
+ 	        	} else {
+ 	        		$('#listNull').hide();
+ 	        	}
+ 	        })
+ 	        .catch(function(e) {
+ 	        	console.warn('error!!! :' + e);
+ 	        });	
+ 		},
+ 		getNextPage: function() {
+ 			this.page += 1;
+ 			this.getList();
+ 		},
+ 		getPrevPage: function() {
+ 			this.page -= 1;
+ 			this.getList();
+ 		},
+ 		readMore: function(blogPostSeq) {
+ 			var detailUrl = '/blog/post/detail/' + blogPostSeq;
+ 			if (this.categorySeq) {
+ 				detailUrl += '?blogPostCategorySeq=' + this.categorySeq;
+ 			}
+ 			location.href = detailUrl;
+ 		}
+	},
+	created: function() {
+		this.getList();
+	}
+});
