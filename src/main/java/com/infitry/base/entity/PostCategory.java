cd627e3dfe0be8fc99fca4e2c1a9e8f2df21bec6
leesw <leesw@DESKTOP-UT2THB4>
2020-04-20 13:08:28 +0900

    [2020-04-20] - thymleaf 문법 수정 - count 변수 추가.

--- src/main/java/com/infitry/base/entity/PostCategory.java
+++ src/main/java/com/infitry/base/entity/PostCategory.java
@@ -15,6 +15,7 @@ import lombok.Data;
 public class PostCategory {
 	private long blogPostCategorySeq;	//카테고리 일련번호
 	private List<BlogPost> blogPosts;	//카테고리 내 포함된 포스트
+	private int count;					//카테고리 내 포함된 포스트 갯 수
 	private String name;				//카테고리명
 	private String regUser;				//생성자
 	private Date regDate;				//생성일
